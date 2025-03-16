40342b11c0464549fc0b155eee5c0fa74596ef54
“Ayush <ayushkumawat77889@gmail.com>
2025-03-16 09:51:04 +0530

    4th commit

--- index.js
+++ index.js
@@ -20,3 +20,10 @@ for(let i=0;i<arr3.length;i++){
   sum3+=arr3[i];
 }
 console.log(sum3);
+
+let arr4=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];   // 4th commit
+let sum4=0;
+for(let i=0;i<arr4.length;i++){
+  sum4+=arr4[i];
+}
+console.log(sum4);
