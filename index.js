a876cf0b5ed5f84d6dbde64a1f8b6936f472a2aa
“Ayush <ayushkumawat77889@gmail.com>
2025-03-16 09:14:02 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-let arr=[1,2,3,4,5,6,7,8,9,10];
+let arr=[1,2,3,4,5,6,7,8,9,10];   // 1st commit 
 let sum=0;
 for(let i=0;i<arr.length;i++){
   sum+=arr[i];
 
 }
 console.log(sum);
+
+
+let arr2=[1,2,3,4,5,6,7,8,9];    // 2nd commit
+let sum2=0;
+for(let i=0;i<arr2.length;i++){
+  sum2+=arr2[i];
+}
+console.log(sum2);
